b3390b8d1c241535c1849e7b7550903dc3ca13e3
cnuma <pignon_09_consonnes@icloud.com>
2026-07-05 18:24:53 +0200

    feat: centraliser l identite de version AquaLook

---
+++ src/BuildInfo.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef AQUALOOK_VERSION
+#define AQUALOOK_VERSION "5.8.0-dev"
+#endif
+
+#ifndef AQUALOOK_BUILD_NUMBER
+#define AQUALOOK_BUILD_NUMBER "local"
+#endif
+
+#ifndef AQUALOOK_GIT_SHA
+#define AQUALOOK_GIT_SHA "unknown"
+#endif
+
+#ifndef AQUALOOK_GIT_BRANCH
+#define AQUALOOK_GIT_BRANCH "unknown"
+#endif
+
+namespace BuildInfo {
+constexpr const char* PRODUCT = "AquaLook";
+constexpr const char* VERSION = AQUALOOK_VERSION;
+constexpr const char* BUILD_NUMBER = AQUALOOK_BUILD_NUMBER;
+constexpr const char* GIT_SHA = AQUALOOK_GIT_SHA;
+constexpr const char* GIT_BRANCH = AQUALOOK_GIT_BRANCH;
+constexpr const char* SIGNATURE = "#cNuma";
+constexpr uint32_t SPLASH_MIN_READ_MS = 1500;
+}
