6e004b32af0f3494c6fe731e20f73ee0d313ff02
cnuma <pignon_09_consonnes@icloud.com>
2026-07-05 08:01:34 +0200

    feat: ajoute l interface du gestionnaire de stockage SD

---
+++ src/StorageManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Arduino.h>
+#include <FS.h>
+#include <SPI.h>
+
+class StorageManager {
+public:
+    void begin();
+    void end();
+
+    bool isSdAvailable() const { return _sdAvailable; }
+    uint8_t cardType() const { return _cardType; }
+    uint64_t cardSizeBytes() const { return _cardSizeBytes; }
+    uint64_t totalBytes() const { return _totalBytes; }
+    uint64_t usedBytes() const { return _usedBytes; }
+
+    bool existsOnSd(const char* path) const;
+    File openOnSd(const char* path, const char* mode = FILE_READ) const;
+    const char* cardTypeName() const;
+
+private:
+    SPIClass _sdSpi{HSPI};
+    bool _sdAvailable = false;
+    uint8_t _cardType = 0;
+    uint64_t _cardSizeBytes = 0;
+    uint64_t _totalBytes = 0;
+    uint64_t _usedBytes = 0;
+};
