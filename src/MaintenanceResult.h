2b67a340edef4a7ca24e5936fc2926fb93da6169
cnuma <pignon_09_consonnes@icloud.com>
2026-07-28 10:39:39 +0200

    feat(ota): extend persisted maintenance result

--- src/MaintenanceResult.h
+++ src/MaintenanceResult.h
@@ -5,12 +5,24 @@
 struct MaintenanceResult {
     bool valid = false;
     bool success = false;
+    bool updateAvailable = false;
+    bool notificationPending = false;
     uint32_t tlsDurationMs = 0U;
     uint32_t recordedUptimeMs = 0U;
     uint32_t minFreeHeap = 0U;
+    uint32_t manifestSize = 0U;
+    uint32_t firmwareSize = 0U;
     char command[24] = "";
     char httpLine[96] = "";
     char detail[128] = "";
+    char installedVersion[24] = "";
+    char availableVersion[24] = "";
+    char channel[16] = "";
+    char target[16] = "";
+    char environment[40] = "";
+    char board[32] = "";
+    char firmwareUrl[192] = "";
+    char sha256[65] = "";
 };
 
 class MaintenanceResultStore {
